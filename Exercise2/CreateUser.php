911c0a9eb0f0e3b0531ddf83927b9404016025f3
Ben Moeller <benlynnmoeller@gmail.com>
2021-12-02 19:45:12 -0600

    completing exercise 2

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -5,10 +5,31 @@ if ($mysqli->connect_errno) {
   printf("Connect failed: %s\n", $mysqli->connect_error);
   exit();
 }
+
 $user_value = $_POST["user_id"];
+
+$in_table = 0;
+$query = "SELECT `user_id` FROM `Users` WHERE user_id='$user_value'";
+$stmt = $mysqli->query($query);
+if($stmt->num_rows > 0) {
+  $in_table = 1;
+}
 
 if($user_value == "") {
   echo "You need to enter a value, blank value not submitted <br><br>";
 }
+elseif ($in_table == 1) {
+  echo "That value is already in the table, try again pls <br><br>";
+}
+else {
+  $instr = "INSERT INTO Users (user_id) 
+  VALUES ('" . $user_value . "')";
+  $mysqli->query($instr);
+  $instr = "SELECT user_id FROM Users";
+  $result = $mysqli->query($instr);
+  echo "Value was entered into the table!<br><br>";
+}
+
 readfile("CreateUser.html");
- ?>
+$mysqli->close();
+?>
