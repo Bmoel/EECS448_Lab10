a8fe473ff4c0c16720d4334c73740283a5fc908a
Ben Moeller <benlynnmoeller@gmail.com>
2021-12-05 15:08:44 -0600

    finished exercise 5

--- Exercise5/ViewUsers.php
+++ Exercise5/ViewUsers.php
@@ -6,6 +6,22 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
+$query = "SELECT `user_id` FROM `Users`";
+$stmt = $mysqli->query($query);
+$user_arr = array();
+while($row = $stmt->fetch_assoc()) {
+    array_push($user_arr,$row["user_id"]);
+}
 
-
+echo "<table border='1' style='font-size:20pt'>";
+    for($i = -1; $i < count($user_arr); $i++) {
+        if($i == -1) {
+            echo "<tr> <td> <b> Usernames </b> </td> </tr>";
+        } else {
+            echo "<tr>";
+            echo "<td>" . $user_arr[$i] . "</td>";
+            echo "</tr>";
+        }
+    }
+echo "</table>";
 ?>
